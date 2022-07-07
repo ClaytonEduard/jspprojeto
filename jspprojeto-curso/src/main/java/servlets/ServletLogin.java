9de1e5aedb14c88b9f84184a5741a5ab14aa1083
ClaytonEduard <clayton_eduard@hotmail.com>
2022-07-07 17:06:03 -0300

    feat: criação da Servlet de login

---
+++ jspprojeto-curso/src/main/java/servlets/ServletLogin.java
@@ -0,0 +1,35 @@
+package servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/ServletLogin") // mapeamento de url que vem da tel
+public class ServletLogin extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public ServletLogin() {
+		super();
+	}
+
+	/* recebe os dados pela url em parametros */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	/* recebe os dados enviado por um fomulario */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		System.out.println(request.getParameter("nome"));
+		System.out.println(request.getParameter("idade"));
+
+	}
+
+}
