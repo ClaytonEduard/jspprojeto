f583fc73ca00645c26b5377d660c21e7534766fd
ClaytonEduard <clayton_eduard@hotmail.com>
2022-07-09 14:09:15 -0300

    :hammer: atribuindo o modellogin

--- jspprojeto-curso/src/main/java/servlets/ServletLogin.java
+++ jspprojeto-curso/src/main/java/servlets/ServletLogin.java
@@ -2,12 +2,13 @@ package servlets;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import model.ModelLogin;
 
 // controlers
 @WebServlet("/ServletLogin") // mapeamento de url que vem da tel
@@ -28,6 +29,20 @@ public class ServletLogin extends HttpServlet {
 	/* recebe os dados enviado por um fomulario */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+
+		if (login != null && !login.isEmpty() && senha != null && senha.isEmpty()) {
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+
+		} else {
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			request.setAttribute("msg", "Informe o login e senha corretamente");
+			redirecionar.forward(request, response);
+		}
 
 	}
 
