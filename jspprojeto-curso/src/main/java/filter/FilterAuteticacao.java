637d480808a983131a25b72994095dc3fd548454
ClaytonEduard <clayton_eduard@hotmail.com>
2022-07-11 08:51:25 -0300

    :triangular_flag_on_post: vinculando com a pagina de erros

--- jspprojeto-curso/src/main/java/filter/FilterAuteticacao.java
+++ jspprojeto-curso/src/main/java/filter/FilterAuteticacao.java
@@ -71,6 +71,9 @@ public class FilterAuteticacao extends HttpFilter implements Filter {
 
 		} catch (Exception e) {
 			e.printStackTrace();
+			RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
+			request.setAttribute("msg", e.getMessage());
+			redirecionar.forward(request, response);
 			try {
 				connection.rollback();
 			} catch (SQLException e1) {
