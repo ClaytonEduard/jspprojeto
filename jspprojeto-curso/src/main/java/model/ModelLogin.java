f04f348af476bdc900948b6ab44da18bc0af898e
ClaytonEduard <clayton_eduard@hotmail.com>
2022-07-19 09:00:49 -0300

    :wrench: implementação de novos campos

--- jspprojeto-curso/src/main/java/model/ModelLogin.java
+++ jspprojeto-curso/src/main/java/model/ModelLogin.java
@@ -5,9 +5,39 @@ import java.io.Serializable;
 public class ModelLogin implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	private Long id;
+	private String nome;
+	private String email;
 	private String login;
 	private String password;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 	public String getLogin() {
 		return login;
