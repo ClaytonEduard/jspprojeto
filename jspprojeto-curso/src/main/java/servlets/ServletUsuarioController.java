ca962706ae1cc0998e90b204e9a29bb1fa59d191
ClaytonEduard <clayton_eduard@hotmail.com>
2022-07-20 16:52:59 -0300

    :recycle: conexao ao metodo editar

--- jspprojeto-curso/src/main/java/servlets/ServletUsuarioController.java
+++ jspprojeto-curso/src/main/java/servlets/ServletUsuarioController.java
@@ -1,5 +1,6 @@
 package servlets;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -9,11 +10,13 @@ import model.ModelLogin;
 
 import java.io.IOException;
 
-/**
- * Servlet implementation class ServletUsuarioController
- */
+import dao.DAOUsuarioRepository;
+
 public class ServletUsuarioController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
+	// instaciando o dao usuario
+	private DAOUsuarioRepository daoUsuarioRepository = new DAOUsuarioRepository();
 
 	public ServletUsuarioController() {
 		super();
@@ -29,20 +32,59 @@ public class ServletUsuarioController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		String id = request.getParameter("id");
-		String nome = request.getParameter("nome");
-		String email = request.getParameter("email");
-		String login = request.getParameter("login");
-		String password = request.getParameter("password");
+		try {
 
-		ModelLogin modelLogin = new ModelLogin();
-		// se o id for diverente de null e diferente de vazio, recebe a Conversao String
-		// pra long senao recebe nulo
-		modelLogin.setId(id != null && !id.isEmpty() ? Long.parseLong(id) : null);
-		modelLogin.setNome(nome);
-		modelLogin.setEmail(email);
-		modelLogin.setLogin(login);
-		modelLogin.setPassword(password);
+			String msg = "Operação realizada com sucesso";
+
+			String id = request.getParameter("id");
+			String nome = request.getParameter("nome");
+			String email = request.getParameter("email");
+			String login = request.getParameter("login");
+			String password = request.getParameter("password");
+
+			ModelLogin modelLogin = new ModelLogin();
+			// se o id for diverente de null e diferente de vazio, recebe a Conversao String
+			// pra long senao recebe nulo
+			modelLogin.setId(id != null && !id.isEmpty() ? Long.parseLong(id) : null);
+			modelLogin.setNome(nome);
+			modelLogin.setEmail(email);
+			modelLogin.setLogin(login);
+			modelLogin.setPassword(password);
+
+			if (daoUsuarioRepository.validarLogin(modelLogin.getLogin()) && modelLogin.getId() == null) {
+				msg = "Já existe um usuário com o mesmo login, informe outro login.";
+			} else {
+				if(modelLogin.isNovo()) {
+					msg = "Gravado com sucesso";
+				}else {
+					msg = "Atualizado com sucesso";
+				}
+				// salva o usuario
+				modelLogin = daoUsuarioRepository.gravarUsuario(modelLogin);
+			}
+
+			// setar a mensagem no dialog
+			request.setAttribute("msg", msg);
+
+			// seta os dados da pagina para que nao sejam perdidos, tambem possibilidade de
+			// editar os dados
+			request.setAttribute("modelLogin", modelLogin);
+
+			// redirecionar para a pagina de usuario
+			// modo 1 de ussar
+			/*
+			 * RequestDispatcher redireciona =
+			 * request.getRequestDispatcher("principal/usuario.jsp");
+			 * redireciona.forward(request, response);
+			 */
+			// modo 2
+			request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
+			request.setAttribute("msg", e.getMessage());
+			redirecionar.forward(request, response);
+		}
 	}
-
 }
