88fd24d815d7536cf6ee268d9020d5e69e1f80b7
ClaytonEduard <clayton_eduard@hotmail.com>
2022-09-02 10:30:44 -0300

    :lipstick: metodo editar com ajax

--- jspprojeto-curso/src/main/java/servlets/ServletUsuarioController.java
+++ jspprojeto-curso/src/main/java/servlets/ServletUsuarioController.java
@@ -26,24 +26,38 @@ public class ServletUsuarioController extends HttpServlet {
 		try {
 			String acao = request.getParameter("acao");
 
-			if (acao != null && !acao.isEmpty() && acao.equalsIgnoreCase(acao)) {
-				
+			if (acao != null && !acao.isEmpty() && acao.equalsIgnoreCase("deletar")) {
+
 				String idUser = request.getParameter("id");
-				
+
 				daoUsuarioRepository.deletarUser(idUser);
-				
+
 				request.setAttribute("msg", "Excluido com sucesso!");
+
+				request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
+
+			} else if (acao != null && !acao.isEmpty() && acao.equalsIgnoreCase("deletarajax")) {
+
+				String idUser = request.getParameter("id");
+
+				daoUsuarioRepository.deletarUser(idUser);
+
+				response.getWriter().write("Excluido com sucesso!");
+				
+			} else {
+				
+				request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
+			
 			}
-			request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
 
 		} catch (Exception e) {
-			
+
 			e.printStackTrace();
 			RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
-			
+
 			request.setAttribute("msg", e.getMessage());
 			redirecionar.forward(request, response);
-		
+
 		}
 
 	}
