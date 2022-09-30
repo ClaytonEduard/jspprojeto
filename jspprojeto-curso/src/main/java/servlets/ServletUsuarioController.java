cc3667ea9e1d32ac95d76d835bf7008d879c22ca
ClaytonEduard <clayton_eduard@hotmail.com>
2022-09-30 10:01:27 -0300

    teste

--- jspprojeto-curso/src/main/java/servlets/ServletUsuarioController.java
+++ jspprojeto-curso/src/main/java/servlets/ServletUsuarioController.java
@@ -14,7 +14,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.ModelLogin;
 
-
 @WebServlet(urlPatterns = { "/ServletUsuarioController/" })
 public class ServletUsuarioController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
