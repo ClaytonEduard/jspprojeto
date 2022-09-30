7f36cb1bac32b17f9fc7a171945568854a8111b9
ClaytonEduard <clayton_eduard@hotmail.com>
2022-09-30 09:51:37 -0300

    corrigindo mapeamento servlet

--- jspprojeto-curso/src/main/java/servlets/ServletUsuarioController.java
+++ jspprojeto-curso/src/main/java/servlets/ServletUsuarioController.java
@@ -15,7 +15,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import model.ModelLogin;
 
 
-//@WebServlet(urlPatterns = { "/ServletUsuarioController" })
+@WebServlet(urlPatterns = { "/ServletUsuarioController/" })
 public class ServletUsuarioController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
