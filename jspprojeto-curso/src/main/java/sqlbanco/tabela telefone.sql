81c5e16199f1ccccb704dd79ce9a345c485fe638
ClaytonEduard <clayton_eduard@hotmail.com>
2022-10-25 09:12:39 -0300

    Sql de criacao da tabela telefone

---
+++ jspprojeto-curso/src/main/java/sqlbanco/tabela telefone.sql
@@ -0,0 +1,10 @@
+﻿create table telefone(
+	id integer not null default nextval('telefone_id_seq'::regclass),
+	numero character varying(50)NOT NULL,
+	usuario_pai_id bigint NOT NULL,
+	usuario_cad_id bigint NOT NULL,
+CONSTRAINT telefone_pkey PRIMARY KEY (id),
+CONSTRAINT usuario_pai_fk FOREIGN KEY (usuario_pai_id)REFERENCES model_login (id),
+ CONSTRAINT usuario_cad_fk FOREIGN KEY (usuario_cad_id)REFERENCES model_login (id)
+
+)
