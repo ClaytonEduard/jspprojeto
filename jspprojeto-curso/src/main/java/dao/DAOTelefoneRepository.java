32bb1de44e51779ad9e5e69b1f980f4a21ddef00
ClaytonEduard <clayton_eduard@hotmail.com>
2022-10-25 09:12:21 -0300

    implementando dao repositoryu do telefone

---
+++ jspprojeto-curso/src/main/java/dao/DAOTelefoneRepository.java
@@ -0,0 +1,78 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import connection.SingleConnectionBanco;
+import model.ModelTelefone;
+
+public class DAOTelefoneRepository {
+
+	private Connection connection;
+	
+	private DAOUsuarioRepository daoUsuarioRepository =  new DAOUsuarioRepository();
+	
+
+	public DAOTelefoneRepository() {
+		connection = SingleConnectionBanco.getConnection();
+
+	}
+
+	public List<ModelTelefone> listFone(Long idUserPai) throws Exception {
+		List<ModelTelefone> telefones = new ArrayList<ModelTelefone>();
+
+		String sql = "select * from telefone where usuario_pai_id = ?";
+		PreparedStatement statement = connection.prepareStatement(sql);
+
+		ResultSet rs = statement.executeQuery();
+
+		while (rs.next()) {
+			ModelTelefone modelTelefone = new ModelTelefone();
+		    modelTelefone.setId(rs.getLong("id"));
+			modelTelefone.setNumero(rs.getString("numero"));
+			modelTelefone.setUsuario_cad_id(daoUsuarioRepository.consultaUsuarioID(rs.getLong("usuario_cad_id")));
+			modelTelefone.setUsuario_pai_id(daoUsuarioRepository.consultaUsuarioID(rs.getLong("usuario_pai_id")));
+					
+					
+			telefones.add(modelTelefone);
+		}
+
+		return telefones;
+
+	}
+
+	public void gravaTelefone(ModelTelefone modelTelefone) throws Exception {
+
+		String sql = "insert into telefone (numero, usuario_pai_id, usuario_cad_id) values (?,?,?)";
+
+		PreparedStatement statement = connection.prepareStatement(sql);
+
+		statement.setString(1, modelTelefone.getNumero());
+		statement.setLong(2, modelTelefone.getUsuario_pai_id().getId());
+		statement.setLong(3, modelTelefone.getUsuario_cad_id().getId());
+
+		statement.execute();
+
+		connection.commit();
+
+	}
+
+	public void deleteFone(Long id) throws Exception {
+
+		String sql = "delete from telefone where id =?";
+
+		PreparedStatement statement = connection.prepareStatement(sql);
+
+		statement.setLong(1, id);
+
+		statement.executeUpdate();
+
+		connection.commit();
+
+	}
+
+}
