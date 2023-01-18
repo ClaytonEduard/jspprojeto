07c5b9df19012d62254c3f3978b4cf1c007b61ab
ClaytonEduard <clayton_eduard@hotmail.com>
2023-01-18 20:14:42 -0300

    Criando a classe util relatorio, para gerar o PDF

---
+++ jspprojeto-curso/src/main/java/util/ReportUtil.java
@@ -0,0 +1,28 @@
+package util;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+
+import jakarta.servlet.ServletContext;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+@SuppressWarnings({ "rawtypes", "serial" }) 
+public class ReportUtil implements Serializable{
+		
+	public byte[] geraRelatorioPDF(List listaDados, String nomeRelatorio, ServletContext servletContext) throws Exception{
+		
+		/* cria a lista de dados que vem do SQL*/
+		JRBeanCollectionDataSource jrben = new JRBeanCollectionDataSource(listaDados);
+		
+		String caminhoJasper = servletContext.getRealPath("relatorio") + File.separator + nomeRelatorio + ".jasper";
+		
+		JasperPrint impressoraJasper = JasperFillManager.fillReport(caminhoJasper, new HashMap<>(), jrben);
+		
+		return JasperExportManager.exportReportToPdf(impressoraJasper);
+	}
+}
